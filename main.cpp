8fdce30ee9ef71540a4b92a956ef9da6c68d56f2
fred kelch <fbk9527@rit.edu>
2013-04-14 12:36:15 -0400

    Updated project settings for new files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 #include <GL/GL.h>
 #include <GL/glut.h>
 
-int main()
+#include <string>
+#include <iostream>
+
+#include "OBJLoader.h"
+
+#define MIN_ARGS 2
+#define OBJP_INDEX 1
+#define MIN_ARG_ERROR "Error: No OBJ file was provided!"
+
+int main( int argc, const char* argv[])
 {
+
+	if(argc < MIN_ARGS)
+	{
+		std::cerr << MIN_ARG_ERROR << std::endl;
+		exit(1);
+	}
+
+	// Extract file name & Read .obj file
+	std::string fname(argv[OBJP_INDEX]);
+	
+	
 }
