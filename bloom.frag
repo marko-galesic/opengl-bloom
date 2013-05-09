f95fe1ebc3a9126771fee46bb46b320e08b89999
Freddy kelch <freddy@Freddys-iMac.local>
2013-05-09 13:37:24 -0400

    Added two more frame buffers.

--- bloom.frag
+++ bloom.frag
@@ -4,8 +4,6 @@
  ******************************************************************************/
 uniform sampler2D fboTexture;
 void main(){
-  
     vec3 light = vec3( texture2D(fboTexture, gl_TexCoord[0].st));
-    light.r = 0.8;
-    gl_FragColor = vec4(1.0 * light,1.0);
+    gl_FragColor = vec4(light,1.0);
 }
--- main.cpp
+++ main.cpp
@@ -16,9 +16,18 @@
 #include "OBJLoader.h"
 #include "ShaderSetup.h"
 
-GLuint fbo;			// The frame buffer object
+GLuint fbo;             // The frame buffer object
 GLuint fbo_depth;		// The depth buffer for the frame buffer object
-GLuint fbo_texture;	// The texture object to write our frame buffer object to
+GLuint init_texture;	// The texture object to write our frame buffer object to
+
+
+GLuint fbo2;            // The second frame buffer object
+GLuint fbo2_depth;      // The second depth buffer object
+GLuint second_texture;  // The second texture object
+
+GLuint fbo3;            // The second frame buffer object
+GLuint fbo3_depth;      // The second depth buffer object
+GLuint third_texture;   // The second texture object
 
 #define WINDOW_H 600
 #define WINDOW_W 800
@@ -37,8 +46,15 @@ GLuint fbo_texture;	// The texture object to write our frame buffer object to
 static GLuint drawList = 0;
 float angle = 0.0;
 
+// The window dimensions
+GLfloat w_width = WINDOW_W;
+GLfloat w_height= WINDOW_H;
+
 // The bloom shader
 GLuint bloom_shader;
+
+// Which fbo is in use
+int fb_in_use = 0;
 
 // Muted material properties
 GLfloat ad_red[]   = { 0.6, 0.0, 0.0, 1.0 };
@@ -63,54 +79,159 @@ extern "C" {
 //------------------------------------------------------------------------------
 void display(){
     
-    /* Render the 3D scene to our FBO.
-       Note - That anything we render to the FBO is also present on the
-       2d FBOTEXTURE object. This is becuase we attached the TEXTURE
-       buffer object to the FBO object.
+    /* Initial Rendering Pass.
+     * No post processing is done here.
      */
     glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
         glPushAttrib(GL_VIEWPORT_BIT | GL_ENABLE_BIT);
-        glViewport(0, 0, WINDOW_W, WINDOW_H);
+        //glViewport(0, 0, w_width, w_height);
         glMatrixMode( GL_PROJECTION );
         glLoadIdentity();
         gluPerspective( 45, 1.0, 1.0, 100.0 );
         glMatrixMode( GL_MODELVIEW );
         glLoadIdentity();
-        gluLookAt( 0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 );
+        gluLookAt( 0.0, 5.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 );
         glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
         glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glPushMatrix();
             glRotatef(angle, 1.0f, 0.0f, 0.0f);
             glCallList(drawList);
+            glFlush();
         glPopMatrix();
         glPopAttrib();
-    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);		        
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
     
     
-    /** Anything rendered here is performend on the framebuffer glut
-     *  created for us. Here, we use the FBO-TEXTURE2d object as a texture
+    
+    
+    
+    
+    
+    
+    
+    /** Post Processing: Pass# 1
+     *  This pass provides the gaussion blur (Horizontal)
      */
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);				
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-    glLoadIdentity();									
-    glTranslatef(0.0f, 0.0f, -2.0f);
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo2);
+    glPushAttrib(GL_VIEWPORT_BIT | GL_ENABLE_BIT);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrtho(-w_width/2.0, w_width/2.0, -w_height/2.0, w_height/2.0, -1.0, 1.0);
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, fbo_texture);
+    glBindTexture(GL_TEXTURE_2D, init_texture);
     
     glUseProgram(bloom_shader);
-    GLuint texIndex = glGetUniformLocation(bloom_shader, "fboTexture");
-    glUniform1i(texIndex, 0);
+    GLuint pass_1 = glGetUniformLocation(bloom_shader, "fboTexture");
+    glUniform1i(pass_1, 0);
     
     glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f);glVertex3f(-1.0f, -1.0f, 0.0f);						        
-        glTexCoord2f(0.0f, 1.0f);glVertex3f(-1.0f, 1.0f, 0.0f);						
-        glTexCoord2f(1.0f, 1.0f);glVertex3f(1.0f, 1.0f, 0.0f);						
-        glTexCoord2f(1.0f, 0.0f);glVertex3f(1.0f, -1.0f, 0.0f);						
+        glTexCoord2f(0.0f, 0.0f);glVertex3f(-w_width/2.0,  -w_height/2.0, 0.5f);
+        glTexCoord2f(0.0f, 1.0f);glVertex3f(-w_width/2.0,   w_height/2.0, 0.5f);
+        glTexCoord2f(1.0f, 1.0f);glVertex3f(w_width/2.0,    w_height/2.0, 0.5f);
+        glTexCoord2f(1.0f, 0.0f);glVertex3f(w_width/2.0,   -w_height/2.0, 0.5f);
     glEnd();
     glBindTexture(GL_TEXTURE_2D, 0);					        
     glFlush();
     glUseProgram(0);
+    glPopAttrib();
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT,0);
+    
+    
+    
+    
+    
+    
+    
+    
+    /** Post Processing: Pass# 2
+     *  This pass provides the gaussion blur (Verticle)
+     */
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo3);
+    glPushAttrib(GL_VIEWPORT_BIT | GL_ENABLE_BIT);
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, second_texture);
+    
+    glUseProgram(bloom_shader);
+    GLuint pass_2 = glGetUniformLocation(bloom_shader, "fboTexture");
+    glUniform1i(pass_2, 0);
+    
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);glVertex3f(-w_width/2.0,  -w_height/2.0, 0.5f);
+    glTexCoord2f(0.0f, 1.0f);glVertex3f(-w_width/2.0,   w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 1.0f);glVertex3f(w_width/2.0,    w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 0.0f);glVertex3f(w_width/2.0,   -w_height/2.0, 0.5f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFlush();
+    glUseProgram(0);
+    glPopAttrib();
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT,0);
+    
+    
+    
+    
+    
+    
+    /** Pass initially rendered image for blur.
+     *  BLEND
+     */
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo2);
+    glPushAttrib(GL_VIEWPORT_BIT | GL_ENABLE_BIT);
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, third_texture);
+    
+    glUseProgram(bloom_shader);
+    GLuint pass_3 = glGetUniformLocation(bloom_shader, "fboTexture");
+    glUniform1i(pass_3, 0);
+    
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);glVertex3f(-w_width/2.0,  -w_height/2.0, 0.5f);
+    glTexCoord2f(0.0f, 1.0f);glVertex3f(-w_width/2.0,   w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 1.0f);glVertex3f(w_width/2.0,    w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 0.0f);glVertex3f(w_width/2.0,   -w_height/2.0, 0.5f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFlush();
+    glUseProgram(0);
+    glPopAttrib();
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT,0);
+    
+    
+    
+    /** Pass initially rendered image for blur.
+     *  FINAL - Draw to the default buffer GLUT created for us
+     */
+    glPushAttrib(GL_VIEWPORT_BIT | GL_ENABLE_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, second_texture);
+    
+    glUseProgram(bloom_shader);
+    GLuint pass_final = glGetUniformLocation(bloom_shader, "fboTexture");
+    glUniform1i(pass_final, 0);
+    
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);glVertex3f(-w_width/2.0,  -w_height/2.0, 0.5f);
+    glTexCoord2f(0.0f, 1.0f);glVertex3f(-w_width/2.0,   w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 1.0f);glVertex3f(w_width/2.0,    w_height/2.0, 0.5f);
+    glTexCoord2f(1.0f, 0.0f);glVertex3f(w_width/2.0,   -w_height/2.0, 0.5f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFlush();
+    glUseProgram(0);
+    glPopAttrib();
 
 }
 
@@ -122,6 +243,8 @@ void display(){
 // Reshape viewport - GLUT callback
 //------------------------------------------------------------------------------
 void reshape( int width, int height ) {
+    w_width = width;
+    w_height= height;
     glViewport( 0, 0, width, height );
 
 }
@@ -168,8 +291,22 @@ void callback(int time){
 void initFrameBufferDepthBuffer(void) {
 	glGenRenderbuffersEXT(1, &fbo_depth); 
 	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, fbo_depth); 
-	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, WINDOW_W, WINDOW_H);
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, w_width, w_height);
     glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo_depth); 
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, 0);
+    
+    
+    glGenRenderbuffersEXT(1, &fbo2_depth);
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, fbo2_depth);
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, w_width, w_height);
+    glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo2_depth);
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, 0);
+    
+    
+    glGenRenderbuffersEXT(1, &fbo3_depth);
+	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, fbo3_depth);
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, w_width, w_height);
+    glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo3_depth);
 	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, 0);
 }
 
@@ -183,9 +320,27 @@ void initFrameBufferDepthBuffer(void) {
 // Initalize the Texture - will be attached to the FBO object
 //------------------------------------------------------------------------------
 void initFrameBufferTexture(void) {
-	glGenTextures(1, &fbo_texture);
-	glBindTexture(GL_TEXTURE_2D, fbo_texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WINDOW_W, WINDOW_H, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glGenTextures(1, &init_texture);
+	glBindTexture(GL_TEXTURE_2D, init_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w_width, w_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+    
+    glGenTextures(1, &second_texture);
+	glBindTexture(GL_TEXTURE_2D, second_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w_width, w_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+    
+    glGenTextures(1, &third_texture);
+	glBindTexture(GL_TEXTURE_2D, third_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w_width, w_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -206,15 +361,40 @@ void initFrameBuffer(void) {
 	initFrameBufferTexture(); 
 	glGenFramebuffersEXT(1, &fbo); 
 	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
-	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, fbo_texture, 0);
+	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, init_texture, 0);
 	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo_depth);
-	GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT); 
-	if (status != GL_FRAMEBUFFER_COMPLETE_EXT) 
+	GLenum fbo_status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+	if (fbo_status != GL_FRAMEBUFFER_COMPLETE_EXT) 
 	{
 		std::cout << "Couldn't create frame buffer" << std::endl;
 		exit(0);
 	}
-	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0); 
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+    
+    glGenFramebuffersEXT(1, &fbo2);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo2);
+	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, second_texture, 0);
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo2_depth);
+	GLenum fbo2_status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+	if (fbo2_status != GL_FRAMEBUFFER_COMPLETE_EXT)
+	{
+		std::cout << "Couldn't create second frame buffer" << std::endl;
+		exit(0);
+	}
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+    
+    
+    glGenFramebuffersEXT(1, &fbo3);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo3);
+	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, third_texture, 0);
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, fbo3_depth);
+	GLenum fbo3_status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+	if (fbo3_status != GL_FRAMEBUFFER_COMPLETE_EXT)
+	{
+		std::cout << "Couldn't create third frame buffer" << std::endl;
+		exit(0);
+	}
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
 }
 
 
@@ -262,7 +442,8 @@ void init( void ) {
 }
 
 
-//
+
+// Main point of execution
 //------------------------------------------------------------------------------
 int main( int argc,  const char *argv[] ) {
 
@@ -280,7 +461,7 @@ int main( int argc,  const char *argv[] ) {
 		// Setup glut
 		glutInit(&argc,(char**)argv);
 		glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH );
-		glutInitWindowSize(WINDOW_W, WINDOW_H);
+		glutInitWindowSize(w_width, w_height);
 		glutInitWindowPosition(WINDOW_X, WINDOW_Y);
 		glutCreateWindow(WINDOW_TITLE);
    
