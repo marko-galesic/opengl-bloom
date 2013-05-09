a08879e3f20667cad7a5877a138e960aff7cdf84
Freddy kelch <freddy@Freddys-iMac.local>
2013-05-09 15:39:14 -0400

    adding blend shader to the mix.

--- blend.fs
+++ blend.fs
@@ -8,8 +8,5 @@ void main()
 {
 	vec4 orig_clr = texture2D( org,  gl_TexCoord[0].st);
 	vec4 blur_clr = texture2D( blur, gl_TexCoord[1].st );
-
-	if(length(orig_clr.xyz) > 0.0)
-		gl_FragColor = orig_clr;
-	gl_FragColor = mix(blur_clr, orig_clr, 0.5);
+	gl_FragColor = orig_clr + blur_clr;
 }
